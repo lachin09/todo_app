012d0956a8c96ab6432f458b5ba9cbf1fe9fde03
lachin <kavkazes@gmail.com>
2024-11-24 22:38:53 +0200

    added new changes

---
+++ to_do_app/lib/data/models/isar_todo.dart
@@ -0,0 +1,35 @@
+/*
+ISAR TO DO MODEL
+
+Convert todo model into isar todo model what we can store in our isar db
+
+
+
+ */
+
+import 'package:isar/isar.dart';
+import 'package:to_do_app/domain/models/todo.dart';
+
+//to generete isar todo object, run:dart run build_runner build
+part 'isar_todo.g.dart';
+
+@collection
+class TodoIsar {
+  Id id = Isar.autoIncrement;
+  late String text;
+  late bool isComplated;
+
+  // convert isar object -> pure todo object to use in pur app
+
+  Todo toDomain() {
+    return Todo(id: id, text: text, isComplated: isComplated);
+  }
+  //convert pure todo object -> isar object to store in isar db.
+
+  static TodoIsar fromDomain(Todo todo) {
+    return TodoIsar()
+      ..id = todo.id
+      ..text = todo.text
+      ..isComplated = todo.isComplated;
+  }
+}
